3936e562467e3fc83749526ef3d40f20599cf479
Ahmadreal92 <ahmadalnajjar666@gmail.com>
2024-07-24 13:27:12 +0200

    fixing some errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import AppRoutes from './AppRoutes';
 import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <>
-      
+      <Loading />
       <Header />
       <AppRoutes />
     </>
--- frontend/src/pages/Parfum/ParfumPage.js
+++ frontend/src/pages/Parfum/ParfumPage.js
@@ -14,67 +14,60 @@ import NotFound from '../../components/NotFound/NotFound';
 export default function ParfumPage() {
     const [parfum, setParfum] = useState({});
     const { id } = useParams();
-    const {addToCart} = useCart ();
-    const navigate =useNavigate();
-const handleAddToCart = () => {
-    addToCart(parfum);
-navigate('/cart');
-}
+    const { addToCart } = useCart();
+    const navigate = useNavigate();
+    const handleAddToCart = () => {
+        addToCart(parfum);
+        navigate('/cart');
+    }
 
     useEffect(() => {
         getById(id).then(setParfum);
     }, [id]);
     return (
-    <>
-        {!parfum? (<NotFound message="Parfum Not Found" linkText="Back to Home Page"/>
-        ) : (
-            <div className={classes.container}>
-            <img className={classes.img}
-                src={`${parfum.imageUrl}`}
-                alt={parfum.name} />
+        <>
+            {!parfum ? (<NotFound message="Parfum Not Found" linkText="Back to Home Page" />
+            ) : (
+                <div className={classes.container}>
+                    <img className={classes.img}
+                        src={`${parfum.imageUrl}`}
+                        alt={parfum.name} />
 
-            <div className={classes.details}>
-                <div className={classes.header}>
-                    <span className={classes.name}>
-                        {parfum.name}</span>
-                    <span
-                        className={`${classes.favorite} 
+                    <div className={classes.details}>
+                        <div className={classes.header}>
+                            <span className={classes.name}>
+                                {parfum.name}</span>
+                            <span
+                                className={`${classes.favorite} 
                         ${parfum.favorite ? '' : classes.not}`}>
-                        ❤
-                    </span>
+                                ❤
+                            </span>
+                        </div>
+                        <div className={classes.rating}>
+                            <StarRating stars={parfum.stars} size={25} />
+
+                        </div>
+
+                        <div className={classes.origins}>
+                            {parfum.origins?.map(origin => (
+                                <span key={origin}>
+                                    {origin}
+                                </span>
+                            ))}
+                        </div>
+
+                        
+
+                        <div className={classes.price}>
+                            <Price price={parfum.price} />
+                        </div>
+
+                        <button onClick={handleAddToCart}>Add to Cart</button>
+
+
+                    </div>
                 </div>
-                <div className={classes.rating}>
-                    <StarRating stars={parfum.stars} size={25} />
-
-                </div>
-
-                <div className={classes.origins}>
-                    {parfum.origins?.map(origin => (
-                        <span key={origin}>
-                            {origin}
-                        </span>
-                    ))}
-                </div>
-
-                <div className={classes.tags}>
-                    {parfum.tags && (
-                        <Tags
-                            tags={parfum.tags.map(tag =>
-                                 ({ name: tag }))}
-                            forParfumPage={true} />
-                    )}
-                </div>
-
-                <div className={classes.price}>
-                    <Price price={parfum.price} />
-                </div>
-
-                <button onClick={handleAddToCart}>Add to Cart</button>
-
-
-            </div>
-        </div>
-        )}
-    </>
+            )}
+        </>
     );
 }
